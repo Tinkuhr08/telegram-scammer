fe05f0c1bbc678e7b8609cf051a57cb81d53fb65
Tinkuhr08 <tinkusrma08@gmail.com>
2025-07-11 21:35:14 +0530

    states.py

---
+++ utils/states.py
@@ -0,0 +1,11 @@
+# utils/states.py
+
+from aiogram.fsm.state import State, StatesGroup
+
+class ReportScammer(StatesGroup):
+    telegram_id = State()
+    upi_id = State()
+    phone_number = State()
+    image = State()
+    proof = State()
+    amount = State()
