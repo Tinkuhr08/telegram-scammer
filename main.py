a99eeb318e2e68230b856bb15c420f5ecbc63ae8
Tinkuhr08 <tinkusrma08@gmail.com>
2025-07-11 22:10:32 +0530

    main.py

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from config import BOT_TOKEN
 from utils.scammer_loader import load_scammer_ids
-from handlers import start  # we'll add more handlers later
+from handlers import start, report, appeal
+
+dp.include_router(start.router)
+dp.include_router(report.router)
+dp.include_router(appeal.router) # we'll add more handlers later
 
 async def main():
     bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
